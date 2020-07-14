b0019d3ebb21ea20f3e3d61176f0483f19594c0e
ShadowWolf29 <00156118@uca.edu.sv>
2020-07-13 22:33:20 -0600

    50%

--- app/src/main/java/com/example/myplace/ui/terms/TermsFragment.kt
+++ app/src/main/java/com/example/myplace/ui/terms/TermsFragment.kt
@@ -1,29 +1,30 @@
-package com.example.myplace.ui.gallery
+package com.example.myplace.ui.terms
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.myplace.R
+import com.example.myplace.ui.terms.TermsViewModel
 
-class GalleryFragment : Fragment() {
+class TermsFragment : Fragment() {
 
-    private lateinit var galleryViewModel: GalleryViewModel
+
+    private lateinit var termsViewModel: TermsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        galleryViewModel =
-            ViewModelProviders.of(this).get(GalleryViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
-        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
-        })
+        this.activity?.title = "Terminos"
+        termsViewModel =
+            ViewModelProviders.of(this).get(termsViewModel::class.java)
+        val root = inflater.inflate(R.layout.freagment_terminos, container, false)
+
         return root
     }
 }
